747e941a7b3606fa70fbee6a975e6b6c89d6c27e
Roberto009893 <taracolo0903@gmail.com>
2024-10-05 06:03:39 +0900

    fix/update version one

--- components/onboarding/CreateFirstTable.tsx
+++ components/onboarding/CreateFirstTable.tsx
@@ -1,43 +1,71 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-interface CreateWorkspaceProps {
-  workspaceName: string;
-  setWorkspaceName: React.Dispatch<React.SetStateAction<string>>; // Correct type for the setter function
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { MagicWandIcon } from "@radix-ui/react-icons";
+
+interface CreateFirstTableProps {
+  setViewComponentKey: React.Dispatch<React.SetStateAction<string>>,
 }
 
-const CreateFirstTable: React.FC<CreateWorkspaceProps> = ({ workspaceName, setWorkspaceName }: { workspaceName: string, setWorkspaceName: React.Dispatch<React.SetStateAction<string>> }) => {
+const ButtonGroup = [
+  {
+    key: 'find_people',
+    txt: "Find people or companies",
+  },
+  {
+    key: 'import_leads',
+    txt: "Import_leads",
+  }
+]
 
-  const [workspace, setWorkspace] = useState<string>(workspaceName);
+const CreateFirstTable: React.FC<CreateFirstTableProps> = ({ setViewComponentKey }: { setViewComponentKey: React.Dispatch<React.SetStateAction<string>> }) => {
+  const router = useRouter();
 
-  useEffect(() => {
-    setWorkspace(workspaceName);
-  }, [])
+  const [selectedBtnKey, setSelectedBtnKey] = useState("find_people");
+  const [content, setContent] = useState("");
+  const [isFocusTextarea, setIsFocusTextarea] = useState(false);
 
   const handleSubmit = () => {
-    if (!workspace) return;
-    setWorkspaceName(workspace);
+    if (!content) return;
+    setViewComponentKey("first_table");
+    router.push("/");
   }
+
   return (
-    <div className="w-full h-[70rem] md:h-full relative bg-white px-[10rem]">
-      <div className="mt-[10rem] w-full max-w-[50rem] mx-auto">
+    <div className="w-full h-[70rem] md:h-full relative bg-white px-[2rem] sm:px-[10rem]">
+      <div className="mt-[10rem] w-full sm:max-w-[80rem] mx-auto">
         <h2 className="text-[2.5rem] font-bold tracking-normal">Create your workspace</h2>
-        <div className="mt-[2.5rem]">
-          <Input type="text" placeholder="My workspace"
-            className="w-full border border-[#E3E3E3] rounded-lg px-[2rem] py-[2.5rem] text-[1.6rem]"
-            value={workspace}
-            onChange={(e) => setWorkspace(e.target.value)} />
+        <p className="text-[1.4rem] text-[#7E7E7E] mt-4">It&lsquos time to get the perfect leads</p>
+        <div className="w-fit mt-[1.5rem] bg-[#F1F5F9] rounded-lg p-2 flex items-center space-x-4 text-[1.4rem]">
+          {
+            ButtonGroup.map((item, index) => (
+              <Button key={index} className={`py-6 px-8 rounded-md text-[1.2rem] hover:cursor-pointer shadow-none ${item.key === selectedBtnKey ? 'bg-white text-[#7E7E7E] hover:bg-white' : 'bg-[#F1F5F9] text-black hover:bg-[#F1F5F9]'}`}
+                onClick={() => setSelectedBtnKey(item.key)}>
+                {item.txt}
+              </Button>
+            ))
+          }
+        </div>
+        <div className={`mt-[2.5rem] w-full h-[15rem] px-5 py-2 flex justify-between space-x-2 border rounded-lg ${isFocusTextarea ? 'border-customBlue' : 'border-[#CDCDCD]'}`}>
+          <MagicWandIcon className="w-6 h-6 text-customBlue flex-grow mt-4" />
+          <div className="w-full h-full max-h-[15rem] overflow-y-auto">
+            <Textarea className="text-[1.2rem] h-full text-black border-none focus-visible:outline-none focus-visible:ring-0 shadow-none"
+              placeholder="Find CEOs of start-ups in the US, that have recently raised and check their pricing"
+              onChange={(e) => setContent(e.target.value)}
+              onFocus={() => setIsFocusTextarea(true)}
+              onBlur={() => setIsFocusTextarea(false)} />
+          </div>
         </div>
       </div>
-      <div className="absolute bottom-[7rem] w-full max-w-[50rem] left-1/2 -translate-x-1/2">
-        <Button className={`w-full text-white rounded-[8px] p-8 text-[1.8rem] font-bold border
-          ${workspace ? 'bg-customBlue border-customBlue hover:bg-white hover:text-customBlue hover:cursor-pointer' : 'bg-[#4B5563] hover:bg-[#4B5563] border-[#4B5563] hover:cursor-not-allowed'}`}
-          // disabled={!workspace}
+      <div className="absolute bottom-[7rem] w-full px-[2rem] max-w-[50rem] left-1/2 -translate-x-1/2">
+        <Button className={`w-full text-white rounded-[8px] p-8 text-[1.8rem] font-bold border shadow-none
+          ${content ? 'bg-customBlue border-customBlue hover:bg-white hover:text-customBlue hover:cursor-pointer' : 'bg-[#F1F5F9] hover:bg-[#F1F5F9] border-[#F1F5F9] hover:cursor-not-allowed'}`}
           onClick={handleSubmit}>
-          Continue
+          Start
         </Button>
       </div>
     </div>
--- components/onboarding/CreateWorkspace.tsx
+++ components/onboarding/CreateWorkspace.tsx
@@ -1,48 +1,43 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { ChevronRightIcon } from "@radix-ui/react-icons";
 
 interface CreateWorkspaceProps {
   workspaceName: string;
   setWorkspaceName: React.Dispatch<React.SetStateAction<string>>, // Correct type for the setter function
-  viewComponentKey: string,
   setViewComponentKey: React.Dispatch<React.SetStateAction<string>>,
 }
 
-const CreateWorkspace: React.FC<CreateWorkspaceProps> = ({ workspaceName, setWorkspaceName, viewComponentKey, setViewComponentKey }:
-  { workspaceName: string, setWorkspaceName: React.Dispatch<React.SetStateAction<string>>, viewComponentKey: string, setViewComponentKey: React.Dispatch<React.SetStateAction<string>> }) => {
+const CreateWorkspace: React.FC<CreateWorkspaceProps> = ({ workspaceName, setWorkspaceName, setViewComponentKey }:
+  { workspaceName: string, setWorkspaceName: React.Dispatch<React.SetStateAction<string>>, setViewComponentKey: React.Dispatch<React.SetStateAction<string>> }) => {
 
-  const [workspace, setWorkspace] = useState<string>(workspaceName);
-
-  console.log(viewComponentKey);
-
-  useEffect(() => {
-    setWorkspace(workspaceName);
-  }, [])
-
-  const handleSubmit = () => {
-    if (!workspace) return;
-    setWorkspaceName(workspace);
+  const handleClickNextBtn = () => {
+    if (!workspaceName) return;
     setViewComponentKey("invite_collaborator");
   }
+
   return (
-    <div className="w-full h-[70rem] md:h-screen relative bg-white px-[10rem]">
-      <div className="mt-[10rem] w-full max-w-[50rem] mx-auto">
+    <div className="w-full h-[70rem] md:h-screen relative bg-white px-[2rem] sm:px-[10rem]">
+      <div className="mt-[10rem] w-full sm:max-w-[80rem] mx-auto">
         <h2 className="text-[2.5rem] font-bold tracking-normal">Create your workspace</h2>
         <div className="mt-[2.5rem]">
           <Input type="text" placeholder="My workspace"
-            className="w-full border border-[#E3E3E3] rounded-lg px-[2rem] py-[2.5rem] text-[1.6rem]"
-            value={workspace}
-            onChange={(e) => setWorkspace(e.target.value)} />
+            className="w-full border border-[#E3E3E3] rounded-lg px-[1.6rem] py-[2rem] text-[1.4rem] focus-visible:ring-0 focus:border-customBlue"
+            value={workspaceName}
+            onChange={(e) => setWorkspaceName(e.target.value)} />
         </div>
       </div>
-      <div className="absolute bottom-[7rem] w-full max-w-[50rem] left-1/2 -translate-x-1/2">
-        <Button className={`w-full text-white rounded-[8px] p-8 text-[1.8rem] font-bold border
-          ${workspace ? 'bg-customBlue border-customBlue hover:bg-white hover:text-customBlue hover:cursor-pointer' : 'bg-[#4B5563] hover:bg-[#4B5563] border-[#4B5563] hover:cursor-not-allowed'}`}
+      <div className="absolute bottom-[7rem] w-full px-[2rem] max-w-[50rem] left-1/2 -translate-x-1/2">
+        <p className="text-center text-[1.2rem] text-[#7E7E7E] hover:cursor-pointer mb-5" onClick={() => setViewComponentKey("invite_collaborator")}>
+          <span>Skip this step</span>
+          <ChevronRightIcon className="w-5 h-5 ml-4 inline-block" />
+        </p>
+        <Button className={`w-full text-white rounded-[8px] p-8 text-[1.8rem] font-bold border shadow-none
+          ${workspaceName ? 'bg-customBlue border-customBlue hover:bg-white hover:text-customBlue hover:cursor-pointer' : 'bg-[#F1F5F9] hover:bg-[#F1F5F9] border-[#F1F5F9] hover:cursor-not-allowed'}`}
           // disabled={!workspace}
-          onClick={handleSubmit}>
+          onClick={handleClickNextBtn}>
           Continue
         </Button>
       </div>
